907ed745d8347a4479438381047e886100cba853
devv777 <kevimbrisola@gmail.com>
2023-07-06 00:07:30 -0300

    Novo Repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Importa o RouterModule
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule // Adiciona o RouterModule às importações
   ],
   providers: [],
   bootstrap: [AppComponent]
